84f360888420691f7c1b57c33e6efeb54259c01c
Jessica Fernandez <jessicafernandez@Jessicas-MacBook-Pro.local>
2020-12-27 15:10:03 -0800

    trying to add a legend to the map

--- static/js/logic.js
+++ static/js/logic.js
@@ -21,22 +21,29 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
   }).addTo(myMap);
 
-  function circleColor(mag){
-      switch(mag){
-        case mag > 1:
-            return "green";
-        case mag < 1 && mag >2:
-            return "yellow"
-        case mag >2 && mag <3:
-            return "orange";
-        case mag >3 && mag <4:
-            return "red";
-        case mag >4 && mag <5:
-            return "purple"
-        case mag >5:
-            return "black"
+//create a function to change the color of circle markers
+  function circleColor(x){
+    if(x < 1){
+          return "green"}
+    else if (x > 1 && x <2){
+        return "yellow"
+    } 
+    else if (x >2 && x <3){
+        return "orange"
       }
-  }
+          
+    else if (x >3 && x <4){
+        return "red"
+    }
+          
+    else if (x >4 && x <5){
+        return "purple"
+      }
+
+    else {
+        return "black"
+    } 
+    }
   
 // Perform a GET request to the query URL
 d3.json(url, function(data) {
@@ -44,17 +51,17 @@ d3.json(url, function(data) {
     console.log(data.features)
     // Get all earthquakes by lat + long
     for (i = 0; i < data.features.length; i++) {
-        // if lat = 
         earthquakeData = data.features[i]
         var lat = earthquakeData.geometry.coordinates[1]
         var long = earthquakeData.geometry.coordinates[0]
         //Get magnitude of earthquakes
         var mag = earthquakeData.properties.mag
         var location = earthquakeData.properties.place
-
+        //Include popups that provide additional information about the earthquake 
+        //when a marker is clicked
         L.circleMarker([lat, long],{
             radius: mag*5,
-            fillColor: circleColor(mag)
+            color: circleColor(mag)
         }).bindPopup(`<h1> MAGNITUDE: ${mag}</h1>
         <h2>NEAREST LOCATION: ${location}</h2>
         <h3>COORDINATES : [${long}, ${lat}] </h3>`)
@@ -62,14 +69,36 @@ d3.json(url, function(data) {
             
     }
  
-    //Include popups that provide additional information about the earthquake 
-    //when a marker is clicked
-
-
-
-
-    //Create a legend that will provide context for your map data
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
     
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+    
+        var div = L.DomUtil.create('div', 'info legend'),
+            mags = [0,1, 2, 3, 4, 5],
+            labels = [];
+    
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < mags.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + circleColor(mags[i] + 1) + '"></i> ' +
+                mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
+        }
+    
+        return div;
+    };
+    
+    legend.addTo(myMap);
   });
  
   
