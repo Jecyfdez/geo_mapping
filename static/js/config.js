c1bf006b266480cea1e30263c2e1b1953711739b
Jessica Fernandez <jessicafernandez@Jessicas-MacBook-Pro.local>
2020-12-27 11:58:38 -0800

    adding html starting code

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiamVjeWZkZXoiLCJhIjoiY2tpanAwaGJrMDIwYTMwbnpsa25vODhxYSJ9.U5QCr_paonKiygZ6Qmx7QA"
---
+++ static/js/logic.js
@@ -0,0 +1,9 @@
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform a GET request to the query URL
+d3.json(url, function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    console.log(data.features);
+  });
+
+// function createFeatures(earthquakeData)
